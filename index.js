acd66cb866198077efdc4e845f497304f8574f34
Maksym <rustam@dataocean.us>
2023-05-15 23:03:41 +0300

    4

--- index.js
+++ index.js
@@ -200,7 +200,7 @@ function openWhatsapp() {
     }
 
     let wTxtEncoded = encodeURI(wTxt);
-    window.open("https://wa.me/919871198882?text=" + wTxtEncoded);
+    window.open("https://wa.me/4083180798?text=" + wTxtEncoded);
   }
 
 
